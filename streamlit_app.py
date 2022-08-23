27bf611af280abfdef421197c39b3fa164b70fa8
Mohammed Shahabuddin <111740137+Shahab-Khatib@users.noreply.github.com>
2022-08-23 21:12:41 +0530

    Pandas imported
    and s3 file fetched

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ streamlit.header('mere favorite khane')
 streamlit.text(' ☕ Chai')
 streamlit.text(' 🥟 toast')
 streamlit.text('main to ghareeb hun mere ma bap mujhe ghareeb janam diye')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
