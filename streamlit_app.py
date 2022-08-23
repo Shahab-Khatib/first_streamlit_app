19ec3b649140b381e33d89ae3495b03eb3b894b9
Mohammed Shahabuddin <111740137+Shahab-Khatib@users.noreply.github.com>
2022-08-23 20:35:51 +0530

    changes in header and emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('Mere mummy ka subah ka nashta Ma sha Allah')
 streamlit.header('mere favorite khane')
-streamlit.text(' :Spinach: Chai')
-streamlit.text('toast')
+streamlit.text(' ☕ Chai')
+streamlit.text(' 🥟 toast')
 streamlit.text('main to ghareeb hun mere ma bap mujhe ghareeb janam diye')
