dca66ec87cb94a9bef688c0b59cb71fc285ce7b5
Mohammed Shahabuddin <111740137+Shahab-Khatib@users.noreply.github.com>
2022-08-23 20:20:02 +0530

    edited with subah ka nashta

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Mother is Healthy Alhamdulillaah')
+streamlit.header('subah ka khana')
+streamlit.text('Chai')
+streamlit.text('toast')
+streamlit.text('main to ghareeb hun mere ma bap mujhe ghareeb janam diye')
