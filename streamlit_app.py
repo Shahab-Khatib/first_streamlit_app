34051779518120a0c3c2f3147dad91f44d41f6de
Mohammed Shahabuddin <111740137+Shahab-Khatib@users.noreply.github.com>
2022-08-24 19:19:20 +0530

    choose own smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 streamlit.title('Mere mummy ka subah ka nashta Ma sha Allah')
 streamlit.header('mere favorite khane')
+streamlit.header('Choose your favorite Smoothie')
 streamlit.text(' ☕ Chai')
 streamlit.text(' 🥟 toast')
+streamlit.text(' 🍔 Burger')
 streamlit.text('main to ghareeb hun mere ma bap mujhe ghareeb janam diye')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
